5db36938a732d6e02ec3037f1008e617c3ba02ec
kaya70875 <shortsforturkey@gmail.com>
2024-12-21 18:17:47 +0300

    create user model

---
+++ models/user.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is Required"],
+  },
+  lastname: {
+    type: String,
+    required: [true, "Last Name is Required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Email is invalid",
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minLenght: [8, "Password must be at least 8 characters"],
+  },
+});
+
+export default mongoose.models.User || mongoose.model("User", userSchema);
