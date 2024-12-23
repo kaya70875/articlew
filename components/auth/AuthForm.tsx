67d01e2d186d0b6f9d3eb412f81b047326252388
kaya70875 <shortsforturkey@gmail.com>
2024-12-23 20:01:50 +0300

    create authform comp

--- components/auth/AuthForm.tsx
+++ components/auth/AuthForm.tsx
@@ -8,12 +8,12 @@ interface AuthFormProps {
 
 export default function AuthForm({ header, desc, children }: AuthFormProps) {
     return (
-        <form className='bg-white rounded-lg w-2/3 max-w-5xl flex flex-col p-24 gap-16 items-center justify-center'>
+        <div className='bg-white rounded-lg w-2/3 max-w-5xl flex flex-col p-24 gap-16 items-center justify-center'>
             <header className='flex flex-col items-center justify-center gap-4'>
                 <h1>{header}</h1>
                 <p>{desc}</p>
             </header>
             {children}
-        </form>
+        </div>
     )
 }
