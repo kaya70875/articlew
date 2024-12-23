60a9c7b5bb8a63295a49a764af50a3fc3036289e
kaya70875 <shortsforturkey@gmail.com>
2024-12-23 20:01:59 +0300

    create inputField comp

--- components/inputs/InputField.tsx
+++ components/inputs/InputField.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 
 interface InputFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
     label : string;
+    type? : 'text' | 'password';
+    error? : string;
 }
 
-export default function InputField({label , ...props} : InputFieldProps) {
+export default function InputField({label , type = 'text' , error, ...props} : InputFieldProps) {
   return (
     <div className='flex flex-col gap-2 w-full'>
       <label className='text-primaryText font-medium text-lg' htmlFor={label}>{label}</label>
-      <input type="text" id={label} {...props} className='p-2 border border-gray-400 w-full rounded-md'/>
+      <input type={type} id={label} {...props} className='p-2 border border-gray-400 w-full rounded-md'/>
+      {error && <p className='text-red-500 text-base'>{error}</p>}
     </div>
   )
 }
