2642cfc04497d9bb817abdba793f38e2d07efb61
kaya70875 <shortsforturkey@gmail.com>
2024-12-23 20:02:08 +0300

    create createuser route

--- app/api/createUser/route.ts
+++ app/api/createUser/route.ts
@@ -4,25 +4,27 @@ import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 
 export async function POST(req : Request) {
-    const {name , email , password} = await req.json();
+    const {email, lastname , name , password } = await req.json();
 
     try {
-        await connectToDB();
+        await connectToDB(); 
 
         const userExist = await user.findOne({email});
 
         if(userExist) {
-            return NextResponse.json({message : 'User Already Exist'}, {status : 400});
+            console.log('User Already Exist');
+            return NextResponse.json({message : 'User Already Exist'}, {status : 409});
         }
 
         const hashedPassword = await bcrypt.hash(password , 10);
-        const currentUser = await user.create({name : name , email : email , password : hashedPassword});
+        const currentUser = await user.create({name : name , lastname : lastname, email : email , password : hashedPassword});
 
         const savedUser = await currentUser.save();
         console.log('Saved User : ', savedUser);
 
         return NextResponse.json({message : 'User Created Successfully'}, {status : 201});
     } catch (error) {
+
         console.log(error);
         return NextResponse.json({message : 'Internal Server Error'}, {status : 500});
     }
