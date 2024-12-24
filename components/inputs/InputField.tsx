e0ae68db9f82d45fcc2a11dafc23af6110da9b9b
kaya70875 <shortsforturkey@gmail.com>
2024-12-24 19:42:53 +0300

    add hover effects to inputField

--- components/inputs/InputField.tsx
+++ components/inputs/InputField.tsx
@@ -10,7 +10,7 @@ export default function InputField({label , type = 'text' , error, ...props} : I
   return (
     <div className='flex flex-col gap-2 w-full'>
       <label className='text-primaryText font-medium text-lg' htmlFor={label}>{label}</label>
-      <input type={type} id={label} {...props} className='p-2 border border-gray-400 w-full rounded-md'/>
+      <input type={type} id={label} {...props} className='p-2 border border-gray-400 w-full rounded-md hover:border-primaryBlue transition-all duration-300 focus:border-primaryBlue outline-none' />
       {error && <p className='text-red-500 text-base'>{error}</p>}
     </div>
   )
