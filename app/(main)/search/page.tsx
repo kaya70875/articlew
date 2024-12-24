b1cf5114218ae8dff0535d50d9804884aedce964
kaya70875 <shortsforturkey@gmail.com>
2024-12-24 21:23:37 +0300

    add google provider

--- app/(main)/search/page.tsx
+++ app/(main)/search/page.tsx
@@ -1,9 +1,13 @@
+'use client';
+
+import { signOut } from 'next-auth/react'
 import React from 'react'
 
 export default function page() {
   return (
     <div>
       Home Page
+      <p onClick={() => signOut()}>Sign Out</p>
     </div>
   )
 }
--- lib/auth.ts
+++ lib/auth.ts
@@ -3,6 +3,7 @@ import { NextAuthOptions } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
 import { connectToDB } from "@/utils/database";
+import Google from 'next-auth/providers/google';
 
 export const authOptions : NextAuthOptions = {
     providers : [
@@ -38,12 +39,42 @@ export const authOptions : NextAuthOptions = {
                     email : currentUser.email,
                 }
             }
-        })
+        }),
+
+        Google({
+            clientId : process.env.GOOGLE_CLIENT_ID!,
+            clientSecret : process.env.GOOGLE_CLIENT_SECRET!,
+        }),
     ],
+    secret : process.env.NEXTAUTH_SECRET,
     session : {
         strategy : 'jwt',
     },
     callbacks : {
+        async signIn({ user, account, profile }) {
+            const { email, name } = user;
+      
+            try {
+              const db = await connectToDB();
+              const usersCollection = db!.collection("users");
+      
+              // Check if user already exists in the database
+              const existingUser = await usersCollection.findOne({ email });
+      
+              if (!existingUser) {
+                // Add new user to the database
+                await usersCollection.insertOne({
+                  name,
+                  email,
+                });
+              }
+      
+              return true; // Allow sign-in
+            } catch (error) {
+              console.error("Error saving user to database:", error);
+              return false; // Deny sign-in if there's an error
+            }
+          },
         async jwt({user , token}) {
             if(user) {
                 token.id = user.id;
@@ -53,6 +84,10 @@ export const authOptions : NextAuthOptions = {
         async session({session , token}){
             session.user.id = token.id;
             return session;
+        },
+        async redirect({url, baseUrl }) {
+            return `${baseUrl}/search`;
         }
+
     }
 }
