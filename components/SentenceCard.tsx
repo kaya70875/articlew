ceabad80ae26b0cb8274689c7130b47220dbf03c
Ahmet Kaya <shortsforturkey@gmail.com>
2024-12-31 14:55:37 +0300

    add word highlighting for SentenceCard and update its style

--- components/SentenceCard.tsx
+++ components/SentenceCard.tsx
@@ -2,12 +2,21 @@ import React from 'react'
 
 interface SentenceCardProps {
     sentence : string;
+    word : string;
 }
 
-export default function SentenceCard({sentence} : SentenceCardProps) {
+export default function SentenceCard({sentence , word} : SentenceCardProps) {
+
+  const highlightedSentence = sentence.replace(
+    new RegExp(`(${word})`, 'gi'),
+    (match) => `<span class="font-bold underline">${match}</span>`
+  )
+
   return (
-    <div className='flex flex-col gap-4 p-4'>
-        {sentence}
+    <div className='flex flex-col gap-4 p-8 bg-white'>
+        <p className='text-lg' dangerouslySetInnerHTML={{__html : highlightedSentence}}></p>
+        <p>Icons</p>
+        <p>Github</p>
     </div>
   )
 }
