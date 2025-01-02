d9bf3323f165740a481bccd4031968848d223208
Ahmet Kaya <shortsforturkey@gmail.com>
2025-01-02 17:38:38 +0300

    implement favorite functionality in SentenceCard component

--- components/SentenceCard.tsx
+++ components/SentenceCard.tsx
@@ -1,27 +1,27 @@
-import { getSourceName } from '@/utils/helpers';
-import { useRouter } from 'next/navigation';
-import React from 'react'
+import { useSentenceCardActions } from '@/hooks/useSentenceCardActions';
+import { getCurrentUser, getSourceName } from '@/utils/helpers';
+import React, { useState } from 'react'
 
 interface SentenceCardProps {
   sentence: string;
   word: string;
-  source : string;
+  source: string;
 }
 
-export default function SentenceCard({ sentence, word , source }: SentenceCardProps) {
+type name = 'Sentences' | 'Expand' | 'Speaker';
+
+export default function SentenceCard({ sentence, word, source }: SentenceCardProps) {
+
+  const { handleFavorites } = useSentenceCardActions();
+  const user = getCurrentUser();
+
+  const [favorite , setFavorite] = useState(false);
 
   const sentenceCardIcons = [
     {
       name: 'Sentences',
-      icon: (<svg width="23" height="22" viewBox="0 0 23 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+      icon: (<svg width="23" height="22" viewBox="0 0 23 22" fill={favorite ? 'text-primary-blue' : 'none'} xmlns="http://www.w3.org/2000/svg">
         <path d="M20.2913 3.50813C19.7805 2.99713 19.1741 2.59177 18.5066 2.31521C17.8392 2.03865 17.1238 1.8963 16.4013 1.8963C15.6788 1.8963 14.9634 2.03865 14.2959 2.31521C13.6285 2.59177 13.022 2.99713 12.5113 3.50813L11.4513 4.56813L10.3913 3.50813C9.3596 2.47643 7.96032 1.89684 6.50129 1.89684C5.04226 1.89684 3.64298 2.47643 2.61129 3.50813C1.5796 4.53982 1 5.93909 1 7.39813C1 8.85716 1.5796 10.2564 2.61129 11.2881L3.67129 12.3481L11.4513 20.1281L19.2313 12.3481L20.2913 11.2881C20.8023 10.7774 21.2076 10.1709 21.4842 9.50348C21.7608 8.83602 21.9031 8.12061 21.9031 7.39813C21.9031 6.67564 21.7608 5.96023 21.4842 5.29277C21.2076 4.62531 20.8023 4.01888 20.2913 3.50813Z" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" />
-      </svg>)
-    },
-
-    {
-      name: 'Favorites',
-      icon: (<svg width="16" height="21" viewBox="0 0 16 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M15 19.6889L8 14.6889L1 19.6889V3.6889C1 3.15847 1.21071 2.64976 1.58579 2.27469C1.96086 1.89962 2.46957 1.6889 3 1.6889H13C13.5304 1.6889 14.0391 1.89962 14.4142 2.27469C14.7893 2.64976 15 3.15847 15 3.6889V19.6889Z" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" />
       </svg>)
     },
     {
@@ -43,8 +43,8 @@ export default function SentenceCard({ sentence, word , source }: SentenceCardPr
   ]
 
   const handleSourceClick = () => {
-    if(typeof window !== 'undefined') {
-      window.open(source , '_blank');
+    if (typeof window !== 'undefined') {
+      window.open(source, '_blank');
     }
   }
 
@@ -53,12 +53,19 @@ export default function SentenceCard({ sentence, word , source }: SentenceCardPr
     (match) => `<span class="font-bold underline">${match}</span>`
   )
 
+  const handleAddToFavorites = async (name: name) => {
+    if (name === 'Sentences') {
+      setFavorite(prev => !prev);
+      await handleFavorites(sentence, user?.id! , favorite ? 'DELETE' : 'POST');
+    }
+  }
+
   return (
     <div className='flex flex-col gap-4 p-8 bg-white'>
       <p className='text-lg' dangerouslySetInnerHTML={{ __html: highlightedSentence }}></p>
       <div className='flex items-center gap-4'>
         {sentenceCardIcons.map((icon, index) => (
-          <div key={index} className='icon cursor-pointer text-primaryText'>{icon.icon}</div>
+          <div key={index} onClick={() => handleAddToFavorites(icon.name as name)} className={`icon cursor-pointer ${favorite && icon.name === 'Sentences' ? 'fill-primaryBlue text-primaryBlue' : 'text-primaryText'}`}>{icon.icon}</div>
         ))}
       </div>
       <p className='text-base text-primaryBlue hover:underline w-full text-right cursor-pointer capitalize' onClick={handleSourceClick}>{getSourceName(source)}</p>
