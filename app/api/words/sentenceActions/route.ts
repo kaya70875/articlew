d640ffed68c887768b6d561e2b9a87c10efa8384
Ahmet Kaya <shortsforturkey@gmail.com>
2025-01-02 17:38:18 +0300

    add API routes for managing favorite sentences

---
+++ app/api/words/sentenceActions/route.ts
@@ -0,0 +1,81 @@
+import { connectToDB } from "@/utils/database";
+import { NextResponse } from "next/server";
+
+const COLLECTION_NAME = "fav_sentences";
+
+export async function POST(req: Request) {
+  const { sentence, userId } = await req.json();
+
+  const db = await connectToDB();
+
+  try {
+    const collection = db?.collection(COLLECTION_NAME);
+    await collection?.createIndex('sentence', { unique: true });
+
+    const results = await collection?.insertOne({
+      sentence,
+      userId,
+      createdAt: new Date(),
+    });
+
+    return NextResponse.json(
+      { message: "Word Added to Favorites", results },
+      { status: 201 }
+    );
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json(
+      { message: "Error Adding Word to Favorites" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET(req : Request) {
+  const { userId } = await req.json();
+
+  const db = await connectToDB();
+
+  try {
+    const collection = db?.collection(COLLECTION_NAME);
+
+    const results = await collection?.find({ userId }).toArray();
+
+    return NextResponse.json(
+      { message: "Words Fetched Successfully", results },
+      { status: 200 }
+    );
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json(
+      { message: "Error Fetching Words" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(req: Request) {
+  const { sentence, userId } = await req.json();
+
+  const db = await connectToDB();
+
+  try {
+    const collection = db?.collection(COLLECTION_NAME);
+
+    const results = await collection?.deleteOne({
+      sentence,
+      userId,
+    });
+
+    return NextResponse.json(
+      { message: "Sentence Removed from Favorites", results },
+      { status: 200 }
+    );
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json(
+      { message: "Error Removing Sentence from Favorites" },
+      { status: 500 }
+    );
+  }
+}
