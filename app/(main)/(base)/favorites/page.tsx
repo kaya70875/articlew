1e948af99d1cdafa3fd41bb430e17ce55e705f02
Ahmet Kaya <shortsforturkey@gmail.com>
2025-01-06 14:24:21 +0300

    implement favorites page with loading state and search functionality

--- app/(main)/(base)/favorites/page.tsx
+++ app/(main)/(base)/favorites/page.tsx
@@ -1,9 +1,25 @@
+'use client';
+
+import FavoritesCard from '@/components/FavoritesCard';
+import InputField from '@/components/inputs/InputField';
+import Loading from '@/components/Loading';
+import useFetch from '@/hooks/useFetch';
+import { FavoriteSentences, Sentence } from '@/types/sentence';
 import React from 'react'
 
 export default function page() {
-  return (
-    <div>
-      
-    </div>
-  )
+
+    const { data, loading, error } = useFetch<FavoriteSentences[]>('/api/words/getFavorites');
+    console.log('data' , data);
+
+    return (
+        <div className='flex flex-col w-full gap-8 p-4'>
+            <header className='w-full'>
+                <h2>Your Favorites</h2>
+                <InputField placeholder='Search in favorites' label='' />
+            </header>
+            <FavoritesCard favorites={data || []} />
+        </div>
+
+    )
 }
