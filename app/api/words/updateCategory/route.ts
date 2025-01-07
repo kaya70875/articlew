2b17b92715b22901105864b8a790396a37498384
Ahmet Kaya <shortsforturkey@gmail.com>
2025-01-07 21:04:55 +0300

    feat: add API routes for updating and retrieving categories for favorite sentences

---
+++ app/api/words/updateCategory/route.ts
@@ -0,0 +1,61 @@
+import { connectToDB } from "@/utils/database";
+import { NextResponse } from "next/server";
+import { ObjectId } from "mongodb";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+
+export async function POST(req: Request) {
+  const { category, sentenceId, userId } = await req.json();
+  try {
+    if (!userId || !category || !sentenceId) {
+      return NextResponse.json(
+        { message: "User ID, category and sentence ID are required" },
+        { status: 400 }
+      );
+    }
+
+    const db = await connectToDB();
+    const collection = db?.collection("fav_sentences");
+    const result = await collection?.updateOne(
+      { _id: new ObjectId(sentenceId), userId },
+      { $set: { categoryId: category } }
+    );
+
+    return NextResponse.json(result, { status: 200 });
+  } catch (e) {
+    return NextResponse.json(
+      { message: "Error updating category" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET(req: Request) {
+  const session = await getServerSession(authOptions);
+  const userId = session?.user?.id;
+
+  const url = new URL(req.url);
+  const category = url.searchParams.get("category");
+
+  try {
+    if (!userId || !category) {
+      return NextResponse.json(
+        { message: "User ID and category are required" },
+        { status: 400 }
+      );
+    }
+
+    const db = await connectToDB();
+    const collection = db?.collection("fav_sentences");
+    const result = await collection
+      ?.find({ categoryId: category, userId })
+      .toArray();
+
+    return NextResponse.json(result, { status: 200 });
+  } catch (e) {
+    return NextResponse.json(
+      { message: "Error updating category" },
+      { status: 500 }
+    );
+  }
+}
