628663659c136f19464ec55ea136741232ea1a42
Ahmet Kaya <shortsforturkey@gmail.com>
2025-01-09 16:28:21 +0300

    feat: add loading state for AI suggestions and improve feedback display and make hero input sticky

--- app/(main)/(base)/search/page.tsx
+++ app/(main)/(base)/search/page.tsx
@@ -15,8 +15,9 @@ export default function page() {
 
   const router = useRouter();
 
-  const {getAISuggestions} = useSentenceCardActions();
-  const [result , setResult] = useState('');
+  const { getAISuggestions } = useSentenceCardActions();
+  const [result, setResult] = useState('');
+  const [aiLoading, setAiLoading] = useState(false);
 
   const params = useSearchParams();
   const currentWord = params.get('word') || '';
@@ -38,6 +39,11 @@ export default function page() {
 
   const handleResults = async () => {
     router.push(`/search?word=${word}&categories=${categories.join(',')}&page=1`);
+    setResult('');
+    setAiLoading(true);
+    const results = await getAISuggestions(word);
+    setResult(results.result);
+    setAiLoading(false);
   };
 
   const handlePageChange = (event: React.ChangeEvent<unknown>, value: number) => {
@@ -48,9 +54,6 @@ export default function page() {
   const handleKeyDown = async (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
       handleResults();
-      const results = await getAISuggestions(word);
-      console.log('res' , results.result);
-      setResult(results.result);
     }
   };
 
@@ -65,20 +68,21 @@ export default function page() {
 
   return (
     <div className='flex flex-col gap-8 w-full'>
-      <header className="top flex flex-col gap-4">
+      <header className="top flex flex-col gap-4 h-full">
         <h3>Welcome Again , {currentUser?.name}</h3>
         <p>Choose a word to get started!</p>
-
-        <div className="input-wrapper relative w-full">
-          <input type="text" className='hero-input w-full' onChange={(e) => setWord(e.target.value)} value={word} onKeyDown={handleKeyDown} />
-          <div className="filter-icon hover:bg-gray-200 p-2 rounded-md flex items-center gap-2 absolute right-8 top-1/2 transform -translate-y-1/2 cursor-pointer" onClick={() => setModalOpen(true)}>
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M4 5L10 5M10 5C10 6.10457 10.8954 7 12 7C13.1046 7 14 6.10457 14 5M10 5C10 3.89543 10.8954 3 12 3C13.1046 3 14 3.89543 14 5M14 5L20 5M4 12H16M16 12C16 13.1046 16.8954 14 18 14C19.1046 14 20 13.1046 20 12C20 10.8954 19.1046 10 18 10C16.8954 10 16 10.8954 16 12ZM8 19H20M8 19C8 17.8954 7.10457 17 6 17C4.89543 17 4 17.8954 4 19C4 20.1046 4.89543 21 6 21C7.10457 21 8 20.1046 8 19Z" stroke="#1f2937" stroke-width="1.5" stroke-linecap="round" />
-            </svg>
-            <p className='font-medium text-base text-primaryText'>FILTER</p>
+      </header>
+      <div className='sticky bg-whitef top-0 z-10 w-full'>
+          <div className="input-wrapper py-4 relative w-full">
+            <input type="text" className='hero-input w-full' onChange={(e) => setWord(e.target.value)} value={word} onKeyDown={handleKeyDown} />
+            <div className="filter-icon hover:bg-gray-200 p-2 rounded-md flex items-center gap-2 absolute right-8 top-1/2 transform -translate-y-1/2 cursor-pointer" onClick={() => setModalOpen(true)}>
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M4 5L10 5M10 5C10 6.10457 10.8954 7 12 7C13.1046 7 14 6.10457 14 5M10 5C10 3.89543 10.8954 3 12 3C13.1046 3 14 3.89543 14 5M14 5L20 5M4 12H16M16 12C16 13.1046 16.8954 14 18 14C19.1046 14 20 13.1046 20 12C20 10.8954 19.1046 10 18 10C16.8954 10 16 10.8954 16 12ZM8 19H20M8 19C8 17.8954 7.10457 17 6 17C4.89543 17 4 17.8954 4 19C4 20.1046 4.89543 21 6 21C7.10457 21 8 20.1046 8 19Z" stroke="#1f2937" stroke-width="1.5" stroke-linecap="round" />
+              </svg>
+              <p className='font-medium text-base text-primaryText'>FILTER</p>
+            </div>
           </div>
         </div>
-      </header>
 
       {loading && <Loading />}
 
@@ -88,11 +92,13 @@ export default function page() {
 
       {data && (
         <div className='flex flex-col gap-8'>
-
-          <header className="ai-feedback bg-lightBlue w-full p-12 flex flex-col">
-            {result && (
-              <p>{result}</p>
-            )}
+          <header className="ai-feedback bg-blue-200 rounded-md w-full p-8 flex flex-col gap-4">
+            <h5>AI Feedback</h5>
+            <div className="content flex flex-col gap-4 rounded-lg px-8 py-12 bg-white">
+              {aiLoading && <Loading />}
+              <p>The word <span className='text-primaryText font-semibold'>{word}</span> is correct and usable in written English.</p>
+              {result && <p className='text-base'>{result}</p>}
+            </div>
           </header>
 
           <article className='flex flex-col gap-8 p-12 bg-lightBlue w-full'>
