1d5ce23f36416a4d1de0d2440a775d2e30b8b8d1
Ahmet Kaya <shortsforturkey@gmail.com>
2025-01-13 20:28:39 +0300

    feat: integrate WordInfoCard component and update API response types

--- app/(main)/(base)/search/page.tsx
+++ app/(main)/(base)/search/page.tsx
@@ -3,9 +3,10 @@
 import FilterModal from '@/components/FilterModal';
 import Loading from '@/components/Loading';
 import SentenceCard from '@/components/SentenceCard';
+import WordInfoCard from '@/components/WordInfoCard';
 import useAPIFetch from '@/hooks/useAPIFetch';
 import { useSentenceCardActions } from '@/hooks/useSentenceCardActions';
-import { FastApiAIResponse, FastApiResponse } from '@/types/sentence';
+import { FastApiAIResponse, FastApiResponse, FastApiWordResponse } from '@/types/sentence';
 import { Pagination } from '@mui/material';
 import { signOut, useSession } from 'next-auth/react';
 import { useRouter, useSearchParams } from 'next/navigation';
@@ -27,7 +28,7 @@ export default function page() {
   const [modalOpen, setModalOpen] = useState(false);
 
   const { data, loading, error } = useAPIFetch<FastApiResponse>(currentWord ? `/sentences/${currentWord}?categories=${categories.join(',')}&page=${page}` : null);
-  const { data:ai, loading:aiLoading, error:aiError } = useAPIFetch<FastApiAIResponse>(currentWord ? `/generate/${currentWord}` : null);
+  const { data: ai, loading: aiLoading, error: aiError } = useAPIFetch<FastApiAIResponse>(currentWord ? `/generate/${currentWord}` : null);
 
   const auth = useSession();
   const currentUser = auth.data?.user;
@@ -57,7 +58,6 @@ export default function page() {
     handleResults();
   }, [categories])
 
-
   return (
     <div className='flex flex-col gap-8 w-full'>
       <header className="top flex flex-col gap-4 h-full">
@@ -65,16 +65,16 @@ export default function page() {
         <p>Choose a word to get started!</p>
       </header>
       <div className='sticky bg-whitef top-0 z-10 w-full'>
-          <div className="input-wrapper py-4 relative w-full">
-            <input type="text" className='hero-input w-full' onChange={(e) => setWord(e.target.value)} value={word} onKeyDown={handleKeyDown} />
-            <div className="filter-icon hover:bg-gray-200 p-2 rounded-md flex items-center gap-2 absolute right-8 top-1/2 transform -translate-y-1/2 cursor-pointer" onClick={() => setModalOpen(true)}>
-              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M4 5L10 5M10 5C10 6.10457 10.8954 7 12 7C13.1046 7 14 6.10457 14 5M10 5C10 3.89543 10.8954 3 12 3C13.1046 3 14 3.89543 14 5M14 5L20 5M4 12H16M16 12C16 13.1046 16.8954 14 18 14C19.1046 14 20 13.1046 20 12C20 10.8954 19.1046 10 18 10C16.8954 10 16 10.8954 16 12ZM8 19H20M8 19C8 17.8954 7.10457 17 6 17C4.89543 17 4 17.8954 4 19C4 20.1046 4.89543 21 6 21C7.10457 21 8 20.1046 8 19Z" stroke="#1f2937" stroke-width="1.5" stroke-linecap="round" />
-              </svg>
-              <p className='font-medium text-base text-primaryText'>FILTER</p>
-            </div>
+        <div className="input-wrapper py-4 relative w-full">
+          <input type="text" className='hero-input w-full' onChange={(e) => setWord(e.target.value)} value={word} onKeyDown={handleKeyDown} />
+          <div className="filter-icon hover:bg-gray-200 p-2 rounded-md flex items-center gap-2 absolute right-8 top-1/2 transform -translate-y-1/2 cursor-pointer" onClick={() => setModalOpen(true)}>
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M4 5L10 5M10 5C10 6.10457 10.8954 7 12 7C13.1046 7 14 6.10457 14 5M10 5C10 3.89543 10.8954 3 12 3C13.1046 3 14 3.89543 14 5M14 5L20 5M4 12H16M16 12C16 13.1046 16.8954 14 18 14C19.1046 14 20 13.1046 20 12C20 10.8954 19.1046 10 18 10C16.8954 10 16 10.8954 16 12ZM8 19H20M8 19C8 17.8954 7.10457 17 6 17C4.89543 17 4 17.8954 4 19C4 20.1046 4.89543 21 6 21C7.10457 21 8 20.1046 8 19Z" stroke="#1f2937" stroke-width="1.5" stroke-linecap="round" />
+            </svg>
+            <p className='font-medium text-base text-primaryText'>FILTER</p>
           </div>
         </div>
+      </div>
 
       {loading && <Loading />}
 
@@ -84,6 +84,8 @@ export default function page() {
 
       {data && (
         <div className='flex flex-col gap-8'>
+          <WordInfoCard currentWord={currentWord} />
+
           <header className="ai-feedback bg-blue-200 rounded-md w-full p-8 flex flex-col gap-4">
             <h5>AI Feedback</h5>
             <div className="content flex flex-col gap-4 rounded-lg px-8 py-12 bg-white">
@@ -91,7 +93,6 @@ export default function page() {
               <p>{ai?.response}</p>
             </div>
           </header>
-
           <article className='flex flex-col gap-8 p-12 bg-lightBlue w-full'>
             <div className='sentence-cards flex flex-col gap-8'>
               {data?.sentences?.map((sentence, index) => (
