621a656db1ce6facd653938840ba23af89a541be
Ahmet Kaya <shortsforturkey@gmail.com>
2025-01-13 20:27:01 +0300

    refactor: update type definitions for FastApi responses and sentence structures

--- types/sentence.ts
+++ types/sentence.ts
@@ -1,38 +1,50 @@
 export type FastApiResponse = {
-    categories : string[] | null;
-    max_length : number;
-    min_lenght : number;
-    order : 'asc' | 'desc';
-    sentences : Sentence[];
-    total_results : number;
-    sort_by : string;
-    word : string;
-}
+  categories: string[] | null;
+  max_length: number;
+  min_lenght: number;
+  order: "asc" | "desc";
+  sentences: Sentence[];
+  total_results: number;
+  sort_by: string;
+  word: string;
+};
 
 export type FastApiAIResponse = {
-    response : string;
-}
+  response: string;
+};
+
+export type FastApiWordResponse = {
+  adjectives: WordInfo[];
+  adverb: WordInfo[];
+  verb: WordInfo[];
+  noun: WordInfo[];
+};
+
+export type WordInfo = {
+  definition: string;
+  synonyms: string[];
+};
 
 export type Sentence = {
-    _id : string;
-    text: string;
-    source: string;
-    category: string;
-    length : number;
-    date : string;
-}
+  _id: string;
+  text: string;
+  source: string;
+  category: string;
+  length: number;
+  date: string;
+};
 
 export type BaseEntity = {
-    _id : string;
-    userId : string;
-    createdAt : Date;
-}
+  _id: string;
+  userId: string;
+  createdAt: Date;
+};
 
 export type FavoriteSentences = BaseEntity & {
-    sentence : string;
-    categoryId : string | null;
-}
+  sentence: string;
+  categoryId: string | null;
+};
 
 export type CategorySentence = BaseEntity & {
-    category : string;
-}
+  category: string;
+};
