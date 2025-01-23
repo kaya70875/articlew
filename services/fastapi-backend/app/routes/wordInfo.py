94a6748337ac39ed0f10fe3c26efe3f1201dc44a
Ahmet Kaya <shortsforturkey@gmail.com>
2025-01-23 18:42:39 +0300

    make wordInfo function async to optimize performance

--- services/fastapi-backend/app/routes/wordInfo.py
+++ services/fastapi-backend/app/routes/wordInfo.py
@@ -2,17 +2,16 @@ from fastapi import APIRouter
 import nltk
 from app.models.word_info import WordInfoResponse
 from word.wordNet import get_word_info_extended
+import asyncio
 
 router = APIRouter()
 
+#Download Libraries
+nltk.download('wordnet')
+nltk.download('omw-1.4')
 
 @router.get("/wordInfo/{word}" , response_model=WordInfoResponse)
 async def get_word_info(word: str):
-
-    #Download Libraries
-    nltk.download('wordnet')
-    nltk.download('omw-1.4')
-
     # Call the function to get word information
-    word_info = get_word_info_extended(word)
+    word_info = await asyncio.to_thread(get_word_info_extended, word)
     return word_info
