9b93d3455c17dda9621d2aff17486ee0642fcc41
Ahmet Kaya <shortsforturkey@gmail.com>
2025-01-27 13:00:02 +0300

    increase axios timeout to 50000ms

--- lib/axoisInstance.ts
+++ lib/axoisInstance.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL : "http://127.0.0.1:8000/api",
-    timeout : 10000,
-    headers : {
-        "Content-Type" : "application/json",
-    },
+  baseURL: "http://127.0.0.1:8000/api",
+  timeout: 10000 * 5,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default axiosInstance;
