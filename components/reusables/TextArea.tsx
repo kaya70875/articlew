5480991e2d1724fa9cb4835855065fdc24ac7b24
Ahmet Kaya <shortsforturkey@gmail.com>
2025-01-27 13:01:35 +0300

    create textarea component for reusability

---
+++ components/reusables/TextArea.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+interface TextAreaProps {
+    textAreaRef: React.RefObject<HTMLTextAreaElement | null>;
+    children?: React.ReactNode;
+}
+
+export default function TextArea({ textAreaRef, children }: TextAreaProps) {
+    return (
+        <div className='wrapper w-full'>
+            <div className="text-area-wrapper bg-white relative w-full p-4 min-h-36 rounded-md">
+                <textarea className='w-full outline-none resize-none text-primaryText' ref={textAreaRef} placeholder='Write a sentence'></textarea>
+                <div className='relative flex gap-8 w-full p-4 items-center justify-end'>
+                    {children}
+                </div>
+            </div>
+        </div>
+    )
+}
