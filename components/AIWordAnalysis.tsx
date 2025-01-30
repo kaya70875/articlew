f806e56eeeed510f3bd82a683f96d1291a3ca7dd
Ahmet Kaya <shortsforturkey@gmail.com>
2025-01-30 13:55:27 +0300

    refactor AIWordAnalysis component to utilize useParseJson hook and display correctness indicators

--- components/AIWordAnalysis.tsx
+++ components/AIWordAnalysis.tsx
@@ -3,6 +3,9 @@ import { FastApiAIResponse } from '@/types/sentence';
 import React from 'react'
 import Loading from './Loading';
 import { highlighWord, prettyAIResponse } from '@/utils/helpers';
+import { useParseJson } from '@/hooks/useParseJson';
+import CorrectIcon from './svg/CorrectIcon';
+import IconWrong from './svg/IconWrong';
 
 interface AIWordAnalysisProps {
     currentWord: string;
@@ -11,13 +14,22 @@ interface AIWordAnalysisProps {
 export default function AIWordAnalysis({ currentWord }: AIWordAnalysisProps) {
 
     const { data: ai, loading: aiLoading, error: aiError } = useAPIFetch<FastApiAIResponse>(currentWord ? `/generate/${currentWord}` : null);
-    const prettierResponse = prettyAIResponse(ai?.response!);
+
+    const jsonResponse = useParseJson(ai?.response!);
+
+    const prettierResponse = prettyAIResponse(jsonResponse?.analysis);
     const highligedResponse = highlighWord(prettierResponse, currentWord, 'text-primaryText');
+
 
     return (
         <div className="content flex flex-col gap-4 rounded-lg p-6 bg-white">
             {aiLoading && <Loading />}
-            <p className='text-base' dangerouslySetInnerHTML={{ __html: highligedResponse }} />
+            {ai && <div className="check flex items-center gap-2">
+                {jsonResponse?.check.includes('is a correct') ? (<CorrectIcon props={{ color: '#AEC976' }} />) : (<IconWrong props={{ color: 'red' }} />)}
+                <p className='text-lg' dangerouslySetInnerHTML={{ __html: highlighWord(jsonResponse?.check, currentWord, 'text-primaryText') }}></p>
+            </div>}
+
+            {ai && <p className='text-base' dangerouslySetInnerHTML={{ __html: highligedResponse }} />}
         </div>
     )
 }
