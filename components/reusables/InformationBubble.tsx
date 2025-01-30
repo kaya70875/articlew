c4aa99317fe1c81fb48f3a3677a50185fac49fb0
Ahmet Kaya <shortsforturkey@gmail.com>
2025-01-30 19:10:59 +0300

    update information bubble

--- components/reusables/InformationBubble.tsx
+++ components/reusables/InformationBubble.tsx
@@ -10,8 +10,8 @@ export default function InformationBubble({ children, information }: Information
         <div className='dropdown-bubble relative flex justify-center group'>
             {children}
 
-            <div className='dropdown-menu absolute mt-10 p-3 rounded-full bg-primaryText opacity-0 transition-opacity group-hover:opacity-100 group-hover:delay-500'>
-                <p className='text-whitef text-sm'>{information}</p>
+            <div className='dropdown-menu min-w-0 absolute mt-9 z-20 p-3 rounded-full bg-primaryText opacity-0 transition-opacity group-hover:opacity-100 group-hover:delay-500'>
+                <p className='text-whitef whitespace-nowrap text-sm'>{information}</p>
             </div>
         </div>
     );
