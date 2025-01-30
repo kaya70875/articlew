5ce2b8f643e2ab3179210ec8c02cc0c504c44c28
Ahmet Kaya <shortsforturkey@gmail.com>
2025-01-30 13:55:20 +0300

    add custom hook useParseJson for parsing JSON strings with error handling

---
+++ hooks/useParseJson.ts
@@ -0,0 +1,23 @@
+import { FastApiAIResponse } from "@/types/sentence";
+import { useEffect, useState } from "react";
+
+export const useParseJson = (json: string) => {
+  const [parsedJson, setParsedJson] = useState<Record<string, any> | null>(
+    null
+  );
+
+  useEffect(() => {
+    try {
+      if (!json) return;
+
+      const jsonString = json.replace(/^.*?```json\n|\n```$/g, "");
+      const response = JSON.parse(jsonString);
+      setParsedJson(response);
+    } catch (error) {
+      console.error("Failed to parse JSON:", error);
+      setParsedJson(null);
+    }
+  }, [json]);
+
+  return parsedJson;
+};
