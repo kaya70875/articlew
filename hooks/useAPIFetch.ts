f9618dbafd116318c0a47cdd0caf8318e2a0d66a
Ahmet Kaya <shortsforturkey@gmail.com>
2025-01-30 15:34:32 +0300

    enhance useAPIFetch hook with custom retry logic for error handling

--- hooks/useAPIFetch.ts
+++ hooks/useAPIFetch.ts
@@ -2,25 +2,37 @@ import useSWR from "swr";
 import axiosInstance from "@/lib/axoisInstance";
 
 // Generic Fetcher Function
-const fetcher = async <T,>(url: string): Promise<T> => {
-    const response = await axiosInstance.get<T>(url);
-    return response.data;
+const fetcher = async <T>(url: string): Promise<T> => {
+  const response = await axiosInstance.get<T>(url);
+  return response.data;
+};
+
+// Custom retry function
+const shouldRetryOnError = (error: any) => {
+  // Don't retry if the error is due to 404
+  if (error.response && error.response.status === 404) {
+    return false;
+  }
+
+  // Retry on all other errors
+  return true;
 };
 
 // Generic Fetch Hook
-const useAPIFetch = <T,>(endpoint: string | null) => {
-    const { data, error, isValidating } = useSWR<T>(endpoint, fetcher, {
-        revalidateOnFocus: false,
-        revalidateOnReconnect: false,
-        revalidateOnMount: true,
-        dedupingInterval : 60000,
-    });
+const useAPIFetch = <T>(endpoint: string | null) => {
+  const { data, error, isValidating } = useSWR<T>(endpoint, fetcher, {
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false,
+    revalidateOnMount: true,
+    dedupingInterval: 60000,
+    shouldRetryOnError: shouldRetryOnError,
+  });
 
-    return {
-        data: data || null, // Return null if no data
-        loading: isValidating,
-        error: error?.message || '',
-    };
+  return {
+    data: data || null, // Return null if no data
+    loading: isValidating,
+    error: error?.message || "",
+  };
 };
 
 export default useAPIFetch;
