3f75f647fa9335c80df237ef9749e6c0e216cff3
Ahmet Kaya <shortsforturkey@gmail.com>
2025-02-06 17:47:57 +0300

    use Card component in AIWordAnalysis component for consistent styling

--- components/AIWordAnalysis.tsx
+++ components/AIWordAnalysis.tsx
@@ -6,6 +6,7 @@ import { highlighWord, prettyAIResponse } from '@/utils/helpers';
 import { useParseJson } from '@/hooks/useParseJson';
 import CorrectIcon from './svg/CorrectIcon';
 import IconWrong from './svg/IconWrong';
+import Card from './cards/Card';
 
 interface AIWordAnalysisProps {
     currentWord: string;
@@ -15,22 +16,21 @@ export default function AIWordAnalysis({ currentWord }: AIWordAnalysisProps) {
 
     const { data: ai, loading: aiLoading, error: aiError } = useAPIFetch<FastApiAIResponse>(currentWord ? `/generate/${currentWord}` : null);
 
-    const jsonResponse = useParseJson(ai?.response);
+    const jsonResponse = useParseJson(ai?.response || '');
 
     const prettierResponse = prettyAIResponse(jsonResponse?.analysis);
     const highligedResponse = highlighWord(prettierResponse, currentWord, 'text-primaryText');
 
 
     return (
-        <div className="content flex flex-col gap-4 rounded-lg p-6 bg-white">
+        <Card gap='1rem' >
             {aiLoading && <Loading />}
-            {aiError && div}
             {ai && <div className="check flex items-center gap-2">
                 {jsonResponse?.check.includes('is a correct') ? (<CorrectIcon props={{ color: '#AEC976' }} />) : (<IconWrong props={{ color: 'red' }} />)}
                 <p className='text-lg' dangerouslySetInnerHTML={{ __html: highlighWord(jsonResponse?.check, currentWord, 'text-primaryText') }}></p>
             </div>}
 
             {ai && <p className='text-base' dangerouslySetInnerHTML={{ __html: highligedResponse }} />}
-        </div>
+        </Card>
     )
 }
