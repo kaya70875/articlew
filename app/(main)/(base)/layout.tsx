2571c6fc52870cb8c079dfe9cfb16ee7d8131326
Ahmet Kaya <shortsforturkey@gmail.com>
2025-02-07 20:40:12 +0300

    refactor layout to use tailwind classes instead of client side hook

--- app/(main)/(base)/layout.tsx
+++ app/(main)/(base)/layout.tsx
@@ -1,20 +1,14 @@
-'use client';
-
 import Sidebar from "@/components/Sidebar";
-import useScreenSize from "@/hooks/useScreenSize";
 
 export default function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-
-    const { isBelow: isTablet } = useScreenSize(1024);
-
     return (
         <main className="bg-main h-full flex">
             <Sidebar />
-            <article className={`${isTablet ? 'ml-[12%]' : 'ml-sidebar-width'} 2xl:px-24 xl:px-16 lg:px-12 md:px-8 py-12 w-full flex items-center justify-center`}>
+            <article className={`ml-0 md:ml-[12%] p-4 lg:ml-sidebar-width 2xl:px-24 xl:px-16 lg:px-12 md:px-8 py-12 w-full flex items-center justify-center`}>
                 {children}
             </article>
         </main>
