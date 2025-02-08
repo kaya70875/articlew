c3fcb3bf36523bfbbc8b57636cb46586e79678d6
Ahmet Kaya <shortsforturkey@gmail.com>
2025-02-08 21:28:49 +0300

    remove conditional rendering

--- lib/axoisInstance.ts
+++ lib/axoisInstance.ts
@@ -1,17 +1,7 @@
 import axios from "axios";
 
-const deployment = process.env.NEXT_PUBLIC_DEPLOYMENT;
-
-if (!deployment) {
-  throw new Error("Deployment not found");
-}
-
 const axiosInstance = axios.create({
-  baseURL: `${
-    deployment !== undefined && deployment === "production"
-      ? process.env.NEXT_PUBLIC_API_URL
-      : "http:127.0.0.1:8000"
-  }/api`,
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
   timeout: 10000 * 5,
   headers: {
     "Content-Type": "application/json",
