5504b44a65f0add05397a164c92b7b9016abc243
Ahmet Kaya <shortsforturkey@gmail.com>
2025-02-10 19:25:32 +0300

    Create a component for information about service

---
+++ components/reusables/BaseInformation.tsx
@@ -0,0 +1,23 @@
+import Image, { StaticImageData } from 'next/image';
+import React from 'react'
+
+interface BaseInformationProps {
+    svgFile: StaticImageData;
+    title?: string;
+    description?: string;
+    svgWidth?: number;
+    svgHeight?: number;
+}
+
+export default function BaseInformation({ svgFile, title, description, svgWidth = 300, svgHeight = 300 }: BaseInformationProps) {
+    return (
+        <div className='flex flex-col gap-12 p-12 items-center justify-center'>
+            <header className='flex flex-col gap-4 text-center'>
+                <h3>{title}</h3>
+                <p className='text-medium max-w-2xl'>{description}</p>
+            </header>
+
+            <Image src={svgFile} alt='svg-file' width={svgWidth} height={svgHeight} />
+        </div>
+    )
+}
