cd807913f13dad3b5e00718475f6c9b9184e641a
Ahmet Kaya <shortsforturkey@gmail.com>
2025-02-10 19:25:40 +0300

    update no results card

--- components/cards/NoResultsCard.tsx
+++ components/cards/NoResultsCard.tsx
@@ -1,16 +1,16 @@
 import Image from 'next/image'
 import React from 'react'
-import noResults from '@/public/images/no-results.png'
+import noResultsSvg from '@/public/illustrations/no-results.svg'
 
 export default function NoResultsCard() {
     return (
-        <div className='p-12 rounded-lg w-full flex flex-col items-center justify-center gap-6'>
+        <div className='p-12 rounded-lg w-full flex flex-col items-center justify-center gap-12'>
             <header className='flex flex-col items-center gap-2'>
                 <h3>No Results.</h3>
-                <p>We are still trying to expand our database. So you can try again later.</p>
+                <p className='text-center'>We are still trying to expand our database. So you can try again later.</p>
             </header>
 
-            <Image src={noResults} alt='no-results' width={300} height={300} />
+            <Image src={noResultsSvg} alt='no-results' width={300} height={300} />
         </div>
     )
 }
