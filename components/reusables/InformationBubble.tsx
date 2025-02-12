5a33dbcac8f27202ec6281c4772b06767c3a74ca
Ahmet Kaya <shortsforturkey@gmail.com>
2025-02-12 18:41:44 +0300

    Fix information bubble shows up below its range

--- components/reusables/InformationBubble.tsx
+++ components/reusables/InformationBubble.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 interface InformationBubbleProps {
     children: React.ReactNode;
@@ -6,11 +6,14 @@ interface InformationBubbleProps {
 }
 
 export default function InformationBubble({ children, information }: InformationBubbleProps) {
+
+    const [isHovered, setIsHovered] = useState(false);
+
     return (
-        <div className='dropdown-bubble relative flex justify-center group'>
+        <div className='dropdown-bubble relative flex justify-center group' onMouseOver={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
             {children}
 
-            <div className='dropdown-menu min-w-0 absolute mt-9 z-20 p-3 rounded-full bg-primaryText opacity-0 transition-opacity group-hover:opacity-100 group-hover:delay-500'>
+            <div className={`dropdown-menu min-w-0 absolute mt-9 z-20 p-3 rounded-full bg-primaryText transition-opacity ${isHovered ? 'group-hover:opacity-100 group-hover:delay-500' : 'opacity-0 invisible'}`}>
                 <p className='text-whitef whitespace-nowrap text-sm'>{information}</p>
             </div>
         </div>
