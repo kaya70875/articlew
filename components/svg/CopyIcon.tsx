dd9fd7af8ecdd61ad99e0b734b1b87b4ac170e01
Ahmet Kaya <shortsforturkey@gmail.com>
2025-02-15 13:32:36 +0300

    add click handler to CopyIcon for toast notification on copy action

--- components/svg/CopyIcon.tsx
+++ components/svg/CopyIcon.tsx
@@ -1,22 +1,31 @@
+'use client';
 import * as React from "react"
 import InformationBubble from "../reusables/InformationBubble"
-const CopyIcon = ({ props }: { props?: React.SVGProps<SVGSVGElement> }) => (
-    <InformationBubble information='Copy to clipboard'>
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width={22}
-            height={22}
-            fill="none"
-            viewBox="0 0 24 24"
-            className={props?.className}
-            {...props}
-        >
-            <g fill="currentColor" fillRule="evenodd" clipRule="evenodd">
-                <path d="M6.25 5.25c0-2.747 2.187-5 4.917-5h6.666c2.73 0 4.917 2.253 4.917 5v8.5c0 2.747-2.187 5-4.917 5a.75.75 0 0 1 0-1.5c1.873 0 3.417-1.553 3.417-3.5v-8.5c0-1.947-1.544-3.5-3.417-3.5h-6.666c-1.873 0-3.417 1.553-3.417 3.5a.75.75 0 0 1-1.5 0Z" />
-                <path d="M1.25 10.25c0-2.747 2.187-5 4.917-5h6.666c2.73 0 4.917 2.253 4.917 5v8.5c0 2.747-2.187 5-4.917 5H6.167c-2.73 0-4.917-2.253-4.917-5v-8.5Zm4.917-3.5c-1.873 0-3.417 1.553-3.417 3.5v8.5c0 1.947 1.544 3.5 3.417 3.5h6.666c1.873 0 3.417-1.553 3.417-3.5v-8.5c0-1.947-1.544-3.5-3.417-3.5H6.167Z" />
-            </g>
-        </svg>
-    </InformationBubble>
+import { useToast } from "@/context/ToastContext";
 
-)
+const CopyIcon = ({ props }: { props?: React.SVGProps<SVGSVGElement> }) => {
+    const { showToast } = useToast();
+
+    return (
+        <InformationBubble information='Copy to clipboard'>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width={22}
+                height={22}
+                fill="none"
+                viewBox="0 0 24 24"
+                className={props?.className}
+                onClick={() => showToast('Copied to clipboard', 'success')}
+                {...props}
+            >
+                <g fill="currentColor" fillRule="evenodd" clipRule="evenodd">
+                    <path d="M6.25 5.25c0-2.747 2.187-5 4.917-5h6.666c2.73 0 4.917 2.253 4.917 5v8.5c0 2.747-2.187 5-4.917 5a.75.75 0 0 1 0-1.5c1.873 0 3.417-1.553 3.417-3.5v-8.5c0-1.947-1.544-3.5-3.417-3.5h-6.666c-1.873 0-3.417 1.553-3.417 3.5a.75.75 0 0 1-1.5 0Z" />
+                    <path d="M1.25 10.25c0-2.747 2.187-5 4.917-5h6.666c2.73 0 4.917 2.253 4.917 5v8.5c0 2.747-2.187 5-4.917 5H6.167c-2.73 0-4.917-2.253-4.917-5v-8.5Zm4.917-3.5c-1.873 0-3.417 1.553-3.417 3.5v8.5c0 1.947 1.544 3.5 3.417 3.5h6.666c1.873 0 3.417-1.553 3.417-3.5v-8.5c0-1.947-1.544-3.5-3.417-3.5H6.167Z" />
+                </g>
+            </svg>
+        </InformationBubble>
+    )
+
+
+}
 export default CopyIcon
