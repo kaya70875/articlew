cd070b826da4b33b6feff5074adcd9d186fbdcb5
Ahmet Kaya <shortsforturkey@gmail.com>
2025-02-15 15:38:34 +0300

    add icon for website and remove unused imports

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,20 +1,21 @@
 import type { Metadata } from "next";
 import "../globals.css";
-import { Open_Sans, Inter, Lora } from "next/font/google";
+import { Lora } from "next/font/google";
 import SessionProvider from "../../providers/SessionProvider";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { ToastProvider } from "@/context/ToastContext";
+import icon from '@/public/images/logo.jpg'
 
 export const metadata: Metadata = {
   title: "Learn With Articles",
   description: "Learn English and write better with articles",
+  icons: {
+    icon: icon.src,
+  }
 };
 
-const openSans = Open_Sans({ subsets: ["latin"] });
-const inter = Inter({ subsets: ["latin"] });
 const lora = Lora({ subsets: ["latin"] });
-
 
 export default async function RootLayout({
   children,
