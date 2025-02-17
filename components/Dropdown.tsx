bc5b7848a9dc6b72beb666bd13b63ab7c7b67322
Ahmet Kaya <shortsforturkey@gmail.com>
2025-02-17 15:10:24 +0300

    refactor Dropdown component to support flexible positioning and padding

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -1,26 +1,55 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import { JSX } from '@emotion/react/jsx-runtime';
 import React, { useState } from 'react'
 
-interface DropdownProps {
+interface DropdownProps extends React.HTMLAttributes<HTMLDivElement> {
     dropdownTitle?: string | JSX.Element;
     addButton?: boolean;
-    position?: 'left' | 'right';
+    position?: 'right' | 'bottom';
+    basePosition?: 'right' | 'left';
+    padding?: string;
     handleNewCategoryClick?: () => void;
     children: React.ReactNode;
 }
 
-export default function Dropdown({ dropdownTitle = 'Dropdown', addButton = false, handleNewCategoryClick, children, position = 'left' }: DropdownProps) {
+export default function Dropdown({ dropdownTitle = 'Dropdown', addButton = false, handleNewCategoryClick, children, position = 'bottom', padding, basePosition = 'left' }: DropdownProps) {
 
     const [isOpen, setIsOpen] = useState(false);
 
+    const dropdownBase = css`
+        position: relative;
+    `
+
+    const dropdownMenuBase = css`
+        position: absolute;
+        padding: ${padding ? padding : '1rem'};
+        background-color: #e2e8f0;
+        border-radius: 12px;
+        min-width: max-content;
+        display: ${isOpen ? 'block' : 'none'};
+        z-index: 20;
+        ${basePosition}: 0;
+    `
+
+    const dropdownMenuDefault = css`
+        ${dropdownMenuBase}
+        top: 100%;
+        margin-top: .5rem;
+    `
+
+    const dropdownMenuRight = css`
+        ${dropdownMenuBase}
+        bottom: 0;
+        left: 100%;
+        margin-left: 1rem;
+    `
+
     return (
-        <div className='dropdown relative'>
+        <div className='dropdown' css={dropdownBase}>
             <button className='relative hover:text-gray-500' onClick={() => setIsOpen(prev => !prev)}>{dropdownTitle}</button>
             <div
-                className={`dropdown-menu absolute top-full mt-2 p-4 bg-slate-200 rounded-md ${position === 'left' ? 'left-0' : 'right-0'
-                    } ${isOpen ? 'block' : 'hidden'}`}
-                style={{ minWidth: 'max-content' }}
-            >
+                className='dropdown-menu' css={position === 'bottom' ? dropdownMenuDefault : dropdownMenuRight}>
                 <ul className='flex flex-col gap-4 w-full'>
                     {
                         addButton && <li onClick={handleNewCategoryClick}>
