7ca8736c4b17b0483ead0f778249c171c7437290
Ahmet Kaya <shortsforturkey@gmail.com>
2025-02-18 19:51:57 +0300

    augment new lastname type for next-auth types

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -6,12 +6,19 @@ declare module "next-auth" {
   interface Session {
     user: {
       id: string;
+      lastname: string;
     } & DefaultSession["user"];
+  }
+
+  interface User {
+    id: string;
+    lastname: string;
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
     id: string;
+    lastname: string;
   }
 }
