572fa9cab038cf2ee54ab07571d86ae506f9b2f3
Ahmet Kaya <shortsforturkey@gmail.com>
2025-02-28 22:02:08 +0300

    create jest config

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: "./",
+});
+
+const customJestConfig = {
+  // Add more custom options if needed
+  testEnvironment: "jsdom",
+};
+
+module.exports = createJestConfig(customJestConfig);
