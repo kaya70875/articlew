23f1df26979192be90c30dde35bc32c2f9df5ead
Ahmet Kaya <shortsforturkey@gmail.com>
2025-02-28 14:07:53 +0300

    update layout and global.css

--- app/(main)/(base)/layout.tsx
+++ app/(main)/(base)/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
         <main className="bg-main h-full flex">
             <Sidebar />
             <article className={`ml-0 md:ml-[12%] p-4 lg:ml-sidebar-width 2xl:px-24 xl:px-16 lg:px-12 md:px-8 pt-4 md:pt-12 pb-12 w-full flex flex-col md:flex-row items-stretch md:items-center justify-normal md:justify-center`}>
-                <div className="block md:hidden bg-main">
+                <div className="block md:hidden">
                     <MobileNavbar />
                 </div>
                 {children}
