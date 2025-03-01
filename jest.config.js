0dfd66a9b6611e7e7d69b3e200083ac9ce3bd318
Ahmet Kaya <shortsforturkey@gmail.com>
2025-03-01 14:01:45 +0300

    install packages

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,10 @@
 const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: "./",
 });
 
 const customJestConfig = {
-  // Add more custom options if needed
   testEnvironment: "jsdom",
 };
 
