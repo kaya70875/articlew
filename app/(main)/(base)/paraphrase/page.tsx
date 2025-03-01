b6fa5b10f8b96226e1f5200779ef8576eac52378
Ahmet Kaya <shortsforturkey@gmail.com>
2025-03-01 15:07:18 +0300

    modularize paraphrase page to test easier and better maintainability

--- app/(main)/(base)/paraphrase/page.tsx
+++ app/(main)/(base)/paraphrase/page.tsx
@@ -14,7 +14,7 @@ import IconShortened from '@/components/svg/IconShortened';
 import IconExpanded from '@/components/svg/IconExpanded';
 import IconAcademic from '@/components/svg/IconAcademic';
 import InformationBubble from '@/components/reusables/InformationBubble';
-import Card from '@/components/cards/Card';
+import Card, { IconProps } from '@/components/cards/Card';
 import { FastApiAIResponse } from '@/types/aiResponse';
 import IconParaphrase from '@/components/svg/IconParaphrase';
 import { useToast } from '@/context/ToastContext';
@@ -22,6 +22,11 @@ import ApiError from '@/components/errors/ApiError';
 
 type Context = 'Casual' | 'Academic' | 'Formal' | 'Sortened' | 'Extended' | 'Poetic';
 const INPUT_LIMIT = 400;
+
+interface ParaphraseCardProps {
+    data: FastApiAIResponse;
+    icons: IconProps[];
+}
 
 export default function Page() {
 
@@ -32,6 +37,55 @@ export default function Page() {
     const { showToast } = useToast();
 
     const { data, loading, error } = useAPIFetch<FastApiAIResponse>(sentence ? `/paraphrase/${encodeURIComponent(sentence)}/${context}` : null);
+
+    const icons = [
+        { icon: <Speaker isSpeaking={false} />, onClick: (sent: string) => speakSentence(sent) },
+        { icon: <CopyIcon />, onClick: (sent: string) => navigator.clipboard.writeText(sent) },
+    ]
+
+    const handleParaphraseClick = () => {
+        if (textAreaRef.current) {
+            if (textAreaRef.current.value.length > INPUT_LIMIT) {
+                return showToast('Input limit exceeded', 'warning');
+            }
+            setSentence(textAreaRef.current.value);
+        }
+    }
+
+    return (
+        <div className='w-full flex items-center justify-center'>
+            <div className='main-container items-center justify-center'>
+                <TextArea textAreaRef={textAreaRef} defaultValue={sentence}>
+                    <ContextButtons context={context} setContext={setContext} />
+                    <div className="premium-button flex items-center justify-center gap-2 cursor-pointer" onClick={handleParaphraseClick}>
+                        <span>Paraphrase</span>
+                        <IconParaphrase />
+                    </div>
+                </TextArea>
+
+                {loading && <Loading />}
+                {error && <ApiError error={error} errorMessage='Failed to paraphrase sentence' />}
+
+                {data && <div className='card-container'>
+                    <EllipseHeader ellipseColor='bg-primaryPurple' text='Rewrites' />
+                    <ParaphraseCards data={data} icons={icons} />
+                </div>}
+            </div>
+        </div>
+    )
+}
+
+export const ParaphraseCards = ({ data, icons }: ParaphraseCardProps) => {
+    return (
+        <div className='w-full flex flex-col gap-4'>
+            {data?.paraphrase.map((sen, index) => (
+                <Card text={sen} icons={icons} key={index} />
+            ))}
+        </div>
+    )
+}
+
+export const ContextButtons = ({ context, setContext }: { context: Context, setContext: (context: Context) => void }) => {
 
     const buttonTypes = [
         {
@@ -56,53 +110,15 @@ export default function Page() {
         }
     ]
 
-    const icons = [
-        { icon: <Speaker isSpeaking={false} />, onClick: (sent: string) => speakSentence(sent) },
-        { icon: <CopyIcon />, onClick: (sent: string) => navigator.clipboard.writeText(sent) },
-    ]
-
-    const handleParaphraseClick = () => {
-        if (textAreaRef.current) {
-            if (textAreaRef.current.value.length > INPUT_LIMIT) {
-                return showToast('Input limit exceeded', 'warning');
-            }
-            setSentence(textAreaRef.current.value);
-        }
-    }
-
     return (
-        <div className='w-full flex items-center justify-center'>
-            <div className='main-container items-center justify-center'>
-                <TextArea textAreaRef={textAreaRef} defaultValue={sentence}>
-                    <div className='flex flex-row-reverse items-center gap-2'>
-                        {buttonTypes.map((buttonType, index) => (
-                            <InformationBubble information={buttonType.name} key={index}>
-                                <button key={index} className={`relative ${context === buttonType.name ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700 hover:bg-gray-300'} transition-all duration-150 ease-in p-2 md:p-3 rounded-full`} onClick={() => setContext(buttonType.name as Context)}>
-                                    {buttonType.icon}
-                                </button>
-                            </InformationBubble>
-                        ))}
-                    </div>
-                    <div className="premium-button flex items-center justify-center gap-2 cursor-pointer" onClick={handleParaphraseClick}>
-                        <span onClick={handleParaphraseClick}>Paraphrase</span>
-                        <IconParaphrase />
-                    </div>
-                </TextArea>
-
-                {loading && <Loading />}
-                {error && <ApiError error={error} errorMessage='Failed to paraphrase sentence' />}
-
-                {data && <div className='card-container'>
-                    <EllipseHeader ellipseColor='bg-primaryPurple' text='Rewrites' />
-
-                    <div className='w-full flex flex-col gap-4'>
-                        {data?.paraphrase.map((sen, index) => (
-                            <Card text={sen} icons={icons} key={index} />
-                        ))}
-                    </div>
-                </div>}
-            </div>
+        <div className='flex flex-row-reverse items-center gap-2'>
+            {buttonTypes.map((buttonType, index) => (
+                <InformationBubble information={buttonType.name} key={index}>
+                    <button key={index} className={`relative ${context === buttonType.name ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700 hover:bg-gray-300'} transition-all duration-150 ease-in p-2 md:p-3 rounded-full`} onClick={() => setContext(buttonType.name as Context)}>
+                        {buttonType.icon}
+                    </button>
+                </InformationBubble>
+            ))}
         </div>
-
     )
 }
