96803afb76f8ec5c7fa15354d33ba1f6e67bd926
Ahmet Kaya <shortsforturkey@gmail.com>
2025-03-07 21:32:26 +0300

    refactor landing card to be more clean

--- components/reusables/LandingCard.tsx
+++ components/reusables/LandingCard.tsx
@@ -1,15 +1,18 @@
 import Image, { StaticImageData } from 'next/image'
 import React from 'react'
 
+const DEFAULT_IMAGE_SIZE = 300;
+
+type ImageProps = {
+    src: StaticImageData;
+    alt?: string;
+    width?: number;
+    height?: number;
+}
 interface LandingCardProps extends React.HTMLAttributes<HTMLDivElement> {
     title: string;
     description: string;
-    image: {
-        src: StaticImageData;
-        alt?: string;
-        width?: number;
-        height?: number;
-    }
+    image: ImageProps;
     variant?: 'light' | 'dark' | 'blue' | 'purple';
     cardDirection?: 'row' | 'row-reverse';
 }
@@ -42,9 +45,9 @@ export default function LandingCard({ title, description, image, variant = 'ligh
     const theme = themes[variant];
 
     return (
-        <div className={`w-full p-4 rounded-xl shadow-lg ${theme.cardBackgroundColor}`} {...props}>
+        <div className={`w-full p-4 rounded-xl shadow-lg ${theme.cardBackgroundColor} ${props.className}`}>
             <div className={`w-full flex flex-col md:flex-${cardDirection} gap-8 md:gap-0 items-center justify-between 2xl:justify-around`}>
-                <Image src={image.src} alt={image.alt ?? ''} width={image.width ?? 300} height={image.height ?? 300} />
+                <Image src={image.src} alt={image.alt ?? 'landing-card-image'} width={image.width ?? DEFAULT_IMAGE_SIZE} height={image.height ?? DEFAULT_IMAGE_SIZE} />
                 <div className="flex flex-col gap-4">
                     <h3 className={`${theme.titleColor}`}>{title}</h3>
                     <p className={`${theme.textColor}`}>{description}</p>
