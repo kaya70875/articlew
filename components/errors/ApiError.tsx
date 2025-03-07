748bd71410746683c32d1ebbf1bd2fd0cea88543
Ahmet Kaya <shortsforturkey@gmail.com>
2025-03-07 21:32:04 +0300

    do not show error component message for 404

--- components/errors/ApiError.tsx
+++ components/errors/ApiError.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 
 type ApiError = {
     errorMessage?: string;
-    error: Error;
+    error: string;
 }
 
 export default function ApiError({ errorMessage, error }: ApiError) {
-    console.error(error)
+    console.log(error);
+    const is404 = error?.includes('404');
+
+    if (is404) return;
+
     return (
         <div className='flex flex-col gap-4 border border-red-500 p-4 rounded-xl'>
             <p className='text-red-500'>{errorMessage}</p>
