8c6c967611bf2fbe0914a1d55d75d6ca7668205d
Ahmet Kaya <shortsforturkey@gmail.com>
2025-03-11 17:16:02 +0300

    Add authenticated axios instance creation function

--- lib/axoisInstance.ts
+++ lib/axoisInstance.ts
@@ -1,11 +1,30 @@
+"use client";
+
 import axios from "axios";
 
+// Create a base axios instance
 const axiosInstance = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
-  timeout: 10000 * 5,
+  timeout: 50000,
   headers: {
     "Content-Type": "application/json",
   },
 });
+
+// Export a function that creates an authenticated instance
+export function createAuthenticatedInstance(
+  accessToken: string | undefined,
+  provider: string | undefined
+) {
+  return axios.create({
+    baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
+    timeout: 50000,
+    headers: {
+      "Content-Type": "application/json",
+      "X-Provider": provider,
+      ...(accessToken ? { Authorization: `Bearer ${accessToken}` } : {}),
+    },
+  });
+}
 
 export default axiosInstance;
