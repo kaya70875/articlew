d18e4b6271d744b06b9a6702a92fd59250c8cd9a
Ahmet Kaya <shortsforturkey@gmail.com>
2025-03-11 17:15:15 +0300

    augment new types to next auth session

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,22 +1,24 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import NextAuth, { DefaultSession } from "next-auth";
 import { JWT } from "next-auth/jwt";
+import { UserType } from "./userTypes";
 
 declare module "next-auth" {
   interface Session {
     user: {
       id: string;
       lastname: string;
+      accessToken?: string;
     } & DefaultSession["user"];
+    accessToken: string;
+    provider: string;
   }
 
   interface User {
     id: string;
     lastname: string;
-  }
-
-  interface DefaultUser {
-    isPremium?: boolean;
+    userType: UserType;
+    accessToken?: string;
   }
 }
 
@@ -24,5 +26,6 @@ declare module "next-auth/jwt" {
   interface JWT {
     id: string;
     lastname: string;
+    accessToken: string;
   }
 }
