6e862af2a5f4e09067ef44f3d22bfce0a29ec92a
Ahmet Kaya <shortsforturkey@gmail.com>
2025-03-12 17:13:15 +0300

    Refactor ApiError component to handle multiple error types with structured error messages and classes

--- components/errors/ApiError.tsx
+++ components/errors/ApiError.tsx
@@ -5,15 +5,51 @@ type ApiError = {
     error: string;
 }
 
-export default function ApiError({ errorMessage, error }: ApiError) {
-    console.log(error);
-    const is404 = error?.includes('404');
+type ErrorKey = 'NOT_FOUND' | 'REQUEST_EXCEEDED' | 'DEFAULT';
 
-    if (is404) return;
+type ErrorType = {
+    code: string;
+    className?: string;
+    message?: string;
+    messageClassName?: string;
+}
+
+export default function ApiError({ errorMessage, error }: ApiError) {
+
+    const ERROR_TYPES: Record<ErrorKey, ErrorType> = {
+        NOT_FOUND: {
+            code: '404',
+        },
+        REQUEST_EXCEEDED: {
+            code: '402',
+            className: 'border-primaryPurple',
+            message: 'Daily request limit exceeded. Consider upgrading your plan or try again tomorrow.',
+            messageClassName: 'font-medium'
+        },
+        DEFAULT: {
+            code: '', //Any status code will show DEFAULT error type.
+            className: 'border-red-500',
+            message: errorMessage,
+            messageClassName: 'text-red-500',
+        },
+    }
+
+    if (!error) return;
+
+    const getErrorType = () => {
+        if (error.includes(ERROR_TYPES.REQUEST_EXCEEDED.code)) return ERROR_TYPES.REQUEST_EXCEEDED;
+        if (error.includes(ERROR_TYPES.NOT_FOUND.code)) return ERROR_TYPES.NOT_FOUND;
+        return ERROR_TYPES.DEFAULT;
+    }
+
+    const errorType = getErrorType();
+
+    // Return null on NOT_FOUND error.
+    if (errorType === ERROR_TYPES.NOT_FOUND) return;
 
     return (
-        <div className='flex flex-col gap-4 border border-red-500 p-4 rounded-xl'>
-            <p className='text-red-500'>{errorMessage}</p>
+        <div className={`flex flex-col gap-4 border ${errorType.className} p-4 rounded-xl`}>
+            <p className={errorType.messageClassName}>{errorType.message}</p>
         </div>
     )
 }
