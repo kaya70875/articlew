08898269920a6b62fc2f92173dbbb398bd3217fd
Ahmet Kaya <shortsforturkey@gmail.com>
2025-03-16 00:06:57 +0300

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export default async function middleware(req : NextRequest) {
-    const protectedRoutes = ['/search'];
-    const currentPath = req.nextUrl.pathname;
+export default async function middleware(req: NextRequest) {
+  const protectedRoutes = ["/search", "/compare", "/fix", "/paraphrase"];
+  const currentPath = req.nextUrl.pathname;
 
-    const isProtectedRoute = protectedRoutes.some((route) => currentPath.includes(route));
+  const isProtectedRoute = protectedRoutes.some((route) =>
+    currentPath.includes(route)
+  );
 
-    if(isProtectedRoute) {
-        const sessionToken = req.cookies.get('next-auth.session-token') || req.cookies.get('__Secure-next-auth.session-token');
+  if (isProtectedRoute) {
+    const sessionToken =
+      req.cookies.get("next-auth.session-token") ||
+      req.cookies.get("__Secure-next-auth.session-token");
 
-        if(!sessionToken) {
-            return Response.redirect(new URL('/login', req.url));
-        }
+    if (!sessionToken) {
+      return Response.redirect(new URL("/login", req.url));
     }
+  }
 
-    return NextResponse.next();
+  return NextResponse.next();
 }
