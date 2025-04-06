db7b929b7d7d4d7beac322c85869743f880bea80
Ahmet Kaya <shortsforturkey@gmail.com>
2025-04-06 14:31:05 +0300

    remove unused variable

--- app/api/words/getFavorites/route.ts
+++ app/api/words/getFavorites/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 
 const COLLECTION_NAME = "fav_sentences";
 
-export async function GET(req: Request) {
+export async function GET() {
   const session = await getServerSession(authOptions);
   const userId = session?.user?.id;
 
