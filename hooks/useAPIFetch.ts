50ae01d96c85b2034f979aa37a06d555f1de3071
Ahmet Kaya <shortsforturkey@gmail.com>
2025-04-06 14:31:35 +0300

    remove try except block from fetcher so swr can handle and return the error itself

--- hooks/useAPIFetch.ts
+++ hooks/useAPIFetch.ts
@@ -7,13 +7,8 @@ import { AuthAxios } from "@/lib/axoisInstance";
 const useAPIFetch = <T>(endpoint: string | null) => {
   const axiosInstance = AuthAxios();
   const fetcher = async (url: string): Promise<T> => {
-    try {
-      const response = await axiosInstance.get<T>(url);
-      return response.data;
-    } catch (error) {
-      console.error("Fetch error:", error);
-      throw error;
-    }
+    const response = await axiosInstance.get<T>(url);
+    return response.data;
   };
 
   const { data, error, isValidating } = useSWR<T>(endpoint, fetcher, {
@@ -26,7 +21,7 @@ const useAPIFetch = <T>(endpoint: string | null) => {
   return {
     data: data || null,
     loading: isValidating,
-    error: error?.message || "",
+    error: error || null,
   };
 };
 
