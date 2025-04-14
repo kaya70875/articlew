0b8f9154a60f6233a1b2df4a0e541fa82636d0d8
Ahmet Kaya <shortsforturkey@gmail.com>
2025-04-14 13:03:38 +0300

    fix: css bug that happens when modal component opens

--- components/ModalComp.tsx
+++ components/ModalComp.tsx
@@ -11,7 +11,7 @@ interface ModalCompProps {
 
 export default function ModalComp({ onClose, modalTitle, createNewCategory, buttonTitle, children }: ModalCompProps) {
     return (
-        <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50'>
+        <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-30'>
             <div className='bg-main flex flex-col gap-4 p-12 rounded-lg w-1/3'>
                 <header className="modal-header flex flex-col gap-4">
                     <div className='flex items-center justify-between w-full'>
