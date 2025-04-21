b82a3da790611979d01d661f19cfd4aab707aa30
Ahmet Kaya <shortsforturkey@gmail.com>
2025-04-21 13:14:16 +0300

    create types for paddle responses

---
+++ types/paddle.ts
@@ -0,0 +1,15 @@
+export interface PaddlePrices {
+  name: string;
+  description: string;
+  price_id: string;
+  amount: string;
+  currency: string;
+  product_id: string;
+}
+
+export interface PaddleSubsctiption {
+  subscription_id: string;
+  next_billed_at: Date;
+  update_url: string;
+  cancel_url: string;
+}
