674d896b3cc4c84b36b1415ccbb655e65f8d18a9
Ahmet Kaya <shortsforturkey@gmail.com>
2025-04-21 13:15:54 +0300

    create premium page

---
+++ app/(main)/(base)/premium/page.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import SubCard from "@/components/cards/SubCard";
+import ApiError from "@/components/errors/ApiError";
+import Loading from "@/components/Loading";
+import useAPIFetch from "@/hooks/useAPIFetch";
+import { PaddlePrices } from "@/types/paddle";
+
+export default function Page() {
+
+    const { data: prices, loading, error } = useAPIFetch<PaddlePrices[]>('/paddle/prices');
+
+    console.log('Products', prices);
+
+    return (
+        <div className='flex flex-col items-center justify-center w-full h-screen gap-4'>
+            {prices?.map((price, index) => (
+                <SubCard key={index} title={price.name} desc={price.description} priceId={price.price_id} amount={`${price.amount.replaceAll('0', '')} ${price.currency.replace('USD', '$')}`} />
+            ))
+            }
+
+            {loading && <Loading />}
+            {error && <ApiError error={error} errorMessage="Error while getting packages" />}
+        </div>
+    )
+}
