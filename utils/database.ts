91b085d40f43701411bdf6000f99b9edba63fbc1
Ahmet Kaya <shortsforturkey@gmail.com>
2025-04-22 16:21:39 +0300

    update database query

--- utils/database.ts
+++ utils/database.ts
@@ -1,28 +1,27 @@
 import mongoose from "mongoose";
 
-
 /**
  * Database connection script.
- * 
+ *
  */
 export const connectToDB = async () => {
-    mongoose.set("strictQuery", true);
+  mongoose.set("strictQuery", true);
 
-    if (mongoose.connection.readyState === 1) {
-        console.log("MongoDB is already connected");
-        return mongoose.connection.db;
-    }
+  if (mongoose.connection.readyState === 1) {
+    console.log("MongoDB is already connected");
+    return mongoose.connection.db;
+  }
 
-    try {
-        await mongoose.connect(process.env.DATABASE_URI!, {
-            dbName: "learn-with-articles",
-            serverSelectionTimeoutMS: 5000,
-        });
+  try {
+    await mongoose.connect(process.env.DATABASE_URI!, {
+      dbName: "learn-with-articles",
+      serverSelectionTimeoutMS: 5000 * 5,
+    });
 
-        console.log("MongoDB connected");
+    console.log("MongoDB connected");
 
-        return mongoose.connection.db;
-    } catch (error) {
-        console.error("MongoDB connection error:", error);
-    }
+    return mongoose.connection.db;
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+  }
 };
