387500e6cba8617ee0f5059c32fe59ba5f502b3d
Ahmet Kaya <shortsforturkey@gmail.com>
2025-04-22 16:21:29 +0300

    conditionally render premium button for different type of users

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -14,6 +14,7 @@ import IconFix from './svg/IconFix'
 import Dropdown from './Dropdown'
 import Profile from './auth/Profile'
 import ProfileIcon from './reusables/ProfileIcon'
+import { useSession } from 'next-auth/react'
 
 export default function Sidebar() {
     const pathname = usePathname();
@@ -49,6 +50,9 @@ export default function Sidebar() {
     const { isBelow: isTablet } = useScreenSize(1024);
     const { isBelow: isMobile } = useScreenSize(768);
 
+    const { data: session } = useSession();
+    const isFree = session?.user.userType === 'Free';
+
     return (
         <div className={`${isMobile ? 'w-full bottom-0' : 'h-screen top-0'} ${isTablet ? 'w-[12%] items-center' : 'w-sidebar-width items-start'} flex flex-col justify-start z-10 p-3 sm:p-6 gap-8 fixed bg-white shadow-[2px_0_10px_-2px_rgba(0,0,0,0.1)]`}>
             <div className='logo hidden md:flex items-center gap-2 cursor-pointer'>
@@ -65,13 +69,13 @@ export default function Sidebar() {
 
                 ))}
 
-                <Link href={'/premium'} className={`premium-button hidden md:flex items-center gap-3 group mt-4 rounded-full md:rounded-xl`}>
+                {isFree && <Link href={'/premium'} className={`premium-button hidden md:flex items-center gap-3 group mt-4 rounded-full md:rounded-xl`}>
                     <div className="icon"><svg width="14" height="20" viewBox="0 0 16 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M8 15C11.866 15 15 11.866 15 8C15 4.13401 11.866 1 8 1C4.13401 1 1 4.13401 1 8C1 11.866 4.13401 15 8 15Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                         <path d="M4.21 13.89L3 23L8 20L13 23L11.79 13.88" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                     </svg></div>
                     <h5 className='hidden lg:block font-medium group'>Go Premium</h5>
-                </Link>
+                </Link>}
 
                 <div className='logout-section absolute bottom-24 hidden md:flex items-center gap-3 cursor-pointer'>
                     <Dropdown padding='2rem' position='right' dropdownTitle={
