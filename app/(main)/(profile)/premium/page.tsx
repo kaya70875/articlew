f9af303b49a0c36f6ad88264b9442bd8af2ba727
Ahmet Kaya <shortsforturkey@gmail.com>
2025-04-26 15:29:52 +0300

    change premium page folder structure and add responsive to it

--- app/(main)/(profile)/premium/page.tsx
+++ app/(main)/(profile)/premium/page.tsx
@@ -7,6 +7,7 @@ import { PaddlePrices } from "@/types/paddle";
 import { useSession } from "next-auth/react";
 import UserVerifiedFlag from "./components/UserVerifiedFlag";
 import FAQ from "./components/FAQ";
+import Footer from "../../components/Footer";
 
 export default function Page() {
     const { data: prices, loading, error } = useAPIFetch<PaddlePrices[]>('/paddle/prices');
@@ -18,12 +19,13 @@ export default function Page() {
         <div className='flex flex-col items-center w-full gap-8'>
             {!userVerified && <UserVerifiedFlag />}
 
-            <div className="w-full h-screen flex flex-col items-center justify-center gap-16">
-                <header className="flex flex-col gap-4 items-center justify-center">
+            <div className="w-full h-full lg:h-screen flex flex-col items-center justify-center gap-16 p-4">
+                <header className="flex flex-col gap-4 items-center justify-center text-center">
                     <h1>Choose Your Plan</h1>
                     <p className="text-2xl font-medium">Unlock Your Writing Potential</p>
                 </header>
-                <div className="sub-cards flex w-full items-center justify-center gap-8">
+                {loading && <Loading />}
+                <div className="sub-cards flex flex-col flex-wrap md:flex-row w-full items-center justify-center gap-8">
                     {prices?.map((price, index) => (
                         <SubCard
                             key={index}
@@ -38,17 +40,18 @@ export default function Page() {
 
             </div>
 
-            <section className="hero-line flex flex-col gap-8 mb-24 items-center justify-center text-center w-full bg-lightBlue p-8 rounded-lg shadow-lg">
+            <section className="hero-line flex flex-col gap-8 mb-0 md:mb-24 items-center justify-center text-center w-full bg-lightBlue p-8 rounded-lg shadow-lg">
                 <div className="flex flex-col gap-4">
                     <h2 className="text-primaryPurple">Unlock your full potential.</h2>
                     <h4 className="max-w-4xl">Access advanced tools, unlimited AI support, and personalized learning to take your English to the next level.</h4>
                 </div>
                 <button className="primary-button" onClick={() => scrollTo(0, 0)}>Start Premium Now</button>
             </section>
-            {loading && <Loading />}
             {error && console.log('Error while getting packages from paddle.')}
 
             <FAQ />
+
+            <Footer />
         </div>
     );
 }
