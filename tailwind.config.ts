96f2fc7cc8f6549c0e95551eacc8b8faae08dbc6
Ahmet Kaya <shortsforturkey@gmail.com>
2025-04-28 12:11:17 +0300

    update default-padding

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,7 +19,7 @@ export default {
       spacing: {
         "navbar-height": "104px",
         "sidebar-width": "200px",
-        "default-padding": "150px",
+        "default-padding": "160px",
       },
       screens: {
         xxs: "320px",
