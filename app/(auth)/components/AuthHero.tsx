b8d1fd5852291966a7ea20c034579a4e92849f94
Ahmet Kaya <shortsforturkey@gmail.com>
2025-08-23 14:47:47 +0300

    feat: add logo brand to AuthHero component

--- app/(auth)/components/AuthHero.tsx
+++ app/(auth)/components/AuthHero.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import SignupFeatures from './SignupFeatures';
+import Link from 'next/link';
+import Image from 'next/image';
+import logo from '@/public/images/logo.svg'
 
 interface AuthHeroProps {
     title: string;
@@ -10,6 +13,12 @@ interface AuthHeroProps {
 export default function AuthHero({ title, desc, featuresSection }: AuthHeroProps) {
     return (
         <div className="flex flex-col space-y-4">
+            <div className="logo">
+                <Link href={'/'} className='flex items-center'>
+                    <Image src={logo} width={32} height={32} alt='logo' />
+                    <span className='font-bold text-lg text-primaryPurple'>Learnwitharticles</span>
+                </Link>
+            </div>
             <h2 className="text-3xl font-bold tracking-tighter md:text-4xl text-gray-900">
                 {title}
             </h2>
