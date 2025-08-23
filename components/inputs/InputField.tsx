ceca8622fa2cbea5f20c2c3ab4015de14a584a0c
Ahmet Kaya <shortsforturkey@gmail.com>
2025-08-23 13:07:51 +0300

    refactor: update input field style and logic

--- components/inputs/InputField.tsx
+++ components/inputs/InputField.tsx
@@ -1,17 +1,26 @@
-import React from 'react'
+import { LucideProps } from 'lucide-react';
+import React, { ForwardRefExoticComponent, RefAttributes } from 'react'
 
 interface InputFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
-    label : string;
-    type? : 'text' | 'password';
-    error? : string;
+  label: string;
+  icon?: React.ReactElement;
+  type?: 'text' | 'password';
+  error?: string;
 }
 
-export default function InputField({label , type = 'text' , error, ...props} : InputFieldProps) {
+export default function InputField({ label, icon, type = 'text', error, ...props }: InputFieldProps) {
   return (
-    <div className='flex flex-col gap-2 w-full'>
-      <label className='text-primaryText font-medium text-lg' htmlFor={label}>{label}</label>
-      <input type={type} id={label} {...props} className='p-2 border border-gray-400 w-full rounded-md hover:border-primaryBlue transition-all duration-300 focus:border-primaryBlue outline-none' />
-      {error && <p className='text-red-500 text-base'>{error}</p>}
+    <div className='flex flex-col w-full'>
+      <label className='text-sm font-medium text-gray-700 mb-1' htmlFor={label}>{label}</label>
+      <div className='relative'>
+        {icon && (
+          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+            {icon}
+          </div>
+        )}
+        <input type={type} id={label} {...props} className='block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500' />
+      </div>
+      {error && <p className='text-red-500 text-sm'>{error}</p>}
     </div>
   )
 }
